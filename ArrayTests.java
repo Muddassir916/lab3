354a5cac924184f2782542033567570c11f778f3
Muddassir916 <mahmadzada@ucsd.edu>
2023-04-20 11:08:52 -0700

    Fixed Tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = { 1, 2, 3, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] { 4, 3, 2, 1 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {4, 5, 6};
+    assertArrayEquals(new int[] {6, 5, 4}, ArrayExamples.reversed(input1));
   }
 }
